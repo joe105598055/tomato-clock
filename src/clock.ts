a62c0e5ccddc14ed3cb8a7dfe75e38562a16088c
joe Huang <qwert30830@gmail.com>
2022-03-10 15:59:57 +0800

    update

---
+++ src/clock.ts
@@ -0,0 +1,60 @@
+import * as vscode from "vscode";
+const format = require("date-fns/format");
+let clock: NodeJS.Timeout;
+
+export function close(status: vscode.StatusBarItem) {
+  status.hide();
+}
+
+export function start(time: number, status: vscode.StatusBarItem) {
+  clock && clearInterval(clock as NodeJS.Timeout);
+  time = time * 60 * 1000;
+  status.show();
+  clock = setInterval(() => {
+    time = time - 1000;
+    status.text = "Tomato Clock: " + String(format(time, "mm:ss"));
+    if (time === 0) {
+      clearInterval(clock as NodeJS.Timeout);
+      vscode.window
+        .showInformationMessage("Take a break.", "5 min", "10 min")
+        .then((select: string | undefined) => {
+          switch (select) {
+            case "5 min":
+              rest(5, status);
+              break;
+            case "10 min":
+              rest(10, status);
+              break;
+          }
+        });
+    }
+  }, 1000);
+}
+
+function rest(time: number, status: vscode.StatusBarItem) {
+  clock && clearInterval(clock as NodeJS.Timeout);
+  time = time * 60 * 1000;
+  status.show();
+  clock = setInterval(() => {
+    time = time - 1000;
+    status.text = "Tomato Clock: " + String(format(time, "mm:ss"));
+    if (time === 0) {
+      clearInterval(clock as NodeJS.Timeout);
+      vscode.window
+        .showInformationMessage("Start working.", "20 min", "25 min", "30 min")
+        .then((select: string | undefined) => {
+          switch (select) {
+            case "20 min":
+              start(20, status);
+              break;
+            case "25 min":
+              start(25, status);
+              break;
+            case "30 min":
+              start(30, status);
+              break;
+          }
+        });
+    }
+  }, 1000);
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,45 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import { start, close } from "./clock";
+
+let status: vscode.StatusBarItem;
+
+enum Command {
+  OPEN = "tomato-clock.open",
+  STOP = "tomato-clock.stop",
+  STATUS_BAR_CLICK = "tomato-clock.statusBarClick",
+}
+function createStatusBarItem(context: vscode.ExtensionContext) {
+  // register a command that is invoked when the status bar, item is clicked.
+  context.subscriptions.push(
+    vscode.commands.registerCommand(Command.STATUS_BAR_CLICK, async () => {
+      vscode.window
+        .showQuickPick(["Start now", "Restart", "Close"], {
+          placeHolder: "Select action what you want",
+        })
+        .then((select) => {
+          if (select === "Close") {
+            close(status);
+          }
+        });
+    })
+  );
+
+  // create a new status bar item that we can now manage
+  status = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 200);
+  status.command = Command.STATUS_BAR_CLICK;
+
+  context.subscriptions.push(status);
+  // item.backgroundColor = new vscode.ThemeColor("statusBarItem.errorBackground");
+  status.tooltip = `Select action what you want`;
+  status.show();
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  createStatusBarItem(context);
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "tomato-clock" is now active!');
@@ -18,8 +53,22 @@ export function activate(context: vscode.ExtensionContext) {
     vscode.window.showInformationMessage("Hello World from tomato-clock!");
   });
 
-  let start = vscode.commands.registerCommand("tomato-clock.start", () => {
-    vscode.window.showInformationMessage("tomato-clock.start start!");
+  const open = vscode.commands.registerCommand(Command.OPEN, () => {
+    vscode.window
+      .showInformationMessage("Choose process perid: ", "20 min", "25 min", "30 min")
+      .then((select: string | undefined) => {
+        switch (select) {
+          case "20 min":
+            start(20, status);
+            break;
+          case "25 min":
+            start(25, status);
+            break;
+          case "30 min":
+            start(30, status);
+            break;
+        }
+      });
   });
 
   let stop = vscode.commands.registerCommand("tomato-clock.stop", () => {
@@ -27,7 +76,7 @@ export function activate(context: vscode.ExtensionContext) {
   });
 
   context.subscriptions.push(disposable);
-  context.subscriptions.push(start);
+  context.subscriptions.push(open);
   context.subscriptions.push(stop);
 }
 
